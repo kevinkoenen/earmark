bb9d971de37e8eab80b57e11e55a6f4c595d04da
Kevin Koenen <k.koenen93@gmail.com>
2021-03-21 15:47:51 +0100

    Fixed service provider

--- source/EarmarkServiceProvider.php
+++ source/EarmarkServiceProvider.php
@@ -2,12 +2,10 @@
 
 namespace Poing\Earmark;
 
-use Illuminate\Database\Eloquent\Factory as EloquentFactory;
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Poing\Earmark\Commands\EarMarkInstall;
-use Poing\Earmark\Http\Controllers\Sequential;
 use Poing\Earmark\Http\Controllers\Serial;
 use Poing\Earmark\Providers\EventServiceProvider;
 
@@ -60,10 +58,6 @@ class EarmarkServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
-        // Register Factories
-        $this->registerEloquentFactoriesFrom(__DIR__.'/database/factories');
-
         // Default Package Configuration
         $this->mergeConfigFrom(__DIR__.'/config/earmark.php', 'earmark');
         $this->mergeConfigFrom(__DIR__.'/config/default.php', 'earmark');
@@ -75,22 +69,6 @@ class EarmarkServiceProvider extends ServiceProvider
         $this->app->singleton('earmark', function () {
             return new Serial;
         });
-
-        //Poing\Earmark\Http\Controllers
-        $this->app->singleton('sequence', function () {
-            return new Sequential;
-        });
         // @codeCoverageIgnoreEnd
-    }
-
-    /**
-     * Register factories.
-     *
-     * @param  string  $path
-     * @return void
-     */
-    protected function registerEloquentFactoriesFrom($path)
-    {
-        $this->app->make(EloquentFactory::class)->load($path);
     }
 }
